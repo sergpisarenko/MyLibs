653cfb5692afb0d32db92388fbfd9aaf87453947
sergpisarenko <sergey.pisarenko@gmail.com>
2015-01-20 15:27:44 +0300

    Sample form

--- Samples/MainForm.Designer.cs
+++ Samples/MainForm.Designer.cs
@@ -28,169 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gbFindList = new System.Windows.Forms.GroupBox();
-            this.numFLCount = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.numFLMaxLen = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lxFLSource = new System.Windows.Forms.ListBox();
-            this.btnFLTest = new System.Windows.Forms.Button();
-            this.tbFLResult = new System.Windows.Forms.TextBox();
+            this.tbExpression = new System.Windows.Forms.TextBox();
+            this.lbExpression = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnEvaluate = new System.Windows.Forms.Button();
             this.lbResult = new System.Windows.Forms.Label();
-            this.gbFindList.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numFLCount)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numFLMaxLen)).BeginInit();
             this.SuspendLayout();
             // 
-            // gbFindList
+            // tbExpression
             // 
-            this.gbFindList.Controls.Add(this.lbResult);
-            this.gbFindList.Controls.Add(this.tbFLResult);
-            this.gbFindList.Controls.Add(this.btnFLTest);
-            this.gbFindList.Controls.Add(this.lxFLSource);
-            this.gbFindList.Controls.Add(this.label2);
-            this.gbFindList.Controls.Add(this.label1);
-            this.gbFindList.Controls.Add(this.numFLMaxLen);
-            this.gbFindList.Controls.Add(this.numFLCount);
-            this.gbFindList.Location = new System.Drawing.Point(12, 12);
-            this.gbFindList.Name = "gbFindList";
-            this.gbFindList.Size = new System.Drawing.Size(479, 346);
-            this.gbFindList.TabIndex = 0;
-            this.gbFindList.TabStop = false;
-            this.gbFindList.Text = "FindList with string elements";
+            this.tbExpression.Location = new System.Drawing.Point(12, 31);
+            this.tbExpression.Name = "tbExpression";
+            this.tbExpression.Size = new System.Drawing.Size(673, 22);
+            this.tbExpression.TabIndex = 0;
+            this.tbExpression.Text = "System.Math.PI < 3.0 ? \"impossible\" : \"correct\"";
             // 
-            // numFLCount
+            // lbExpression
             // 
-            this.numFLCount.Location = new System.Drawing.Point(51, 21);
-            this.numFLCount.Maximum = new decimal(new int[] {
-            1000000,
-            0,
-            0,
-            0});
-            this.numFLCount.Minimum = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
-            this.numFLCount.Name = "numFLCount";
-            this.numFLCount.Size = new System.Drawing.Size(73, 22);
-            this.numFLCount.TabIndex = 0;
-            this.numFLCount.Value = new decimal(new int[] {
-            10000,
-            0,
-            0,
-            0});
+            this.lbExpression.AutoSize = true;
+            this.lbExpression.Location = new System.Drawing.Point(9, 15);
+            this.lbExpression.Name = "lbExpression";
+            this.lbExpression.Size = new System.Drawing.Size(62, 13);
+            this.lbExpression.TabIndex = 1;
+            this.lbExpression.Text = "Expression";
             // 
-            // label1
+            // tbResult
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Count";
+            this.tbResult.Location = new System.Drawing.Point(12, 102);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(673, 22);
+            this.tbResult.TabIndex = 0;
             // 
-            // numFLMaxLen
+            // btnEvaluate
             // 
-            this.numFLMaxLen.Location = new System.Drawing.Point(237, 21);
-            this.numFLMaxLen.Maximum = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            this.numFLMaxLen.Minimum = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
-            this.numFLMaxLen.Name = "numFLMaxLen";
-            this.numFLMaxLen.Size = new System.Drawing.Size(45, 22);
-            this.numFLMaxLen.TabIndex = 0;
-            this.numFLMaxLen.Value = new decimal(new int[] {
-            15,
-            0,
-            0,
-            0});
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Max. string length";
-            // 
-            // lxFLSource
-            // 
-            this.lxFLSource.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lxFLSource.FormattingEnabled = true;
-            this.lxFLSource.IntegralHeight = false;
-            this.lxFLSource.Location = new System.Drawing.Point(6, 49);
-            this.lxFLSource.Name = "lxFLSource";
-            this.lxFLSource.Size = new System.Drawing.Size(464, 199);
-            this.lxFLSource.TabIndex = 2;
-            // 
-            // btnFLTest
-            // 
-            this.btnFLTest.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFLTest.Location = new System.Drawing.Point(289, 21);
-            this.btnFLTest.Name = "btnFLTest";
-            this.btnFLTest.Size = new System.Drawing.Size(181, 23);
-            this.btnFLTest.TabIndex = 3;
-            this.btnFLTest.Text = "Test!";
-            this.btnFLTest.UseVisualStyleBackColor = true;
-            this.btnFLTest.Click += new System.EventHandler(this.btnFLTest_Click);
-            // 
-            // tbFLResult
-            // 
-            this.tbFLResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbFLResult.Location = new System.Drawing.Point(6, 272);
-            this.tbFLResult.Multiline = true;
-            this.tbFLResult.Name = "tbFLResult";
-            this.tbFLResult.Size = new System.Drawing.Size(464, 68);
-            this.tbFLResult.TabIndex = 4;
+            this.btnEvaluate.Location = new System.Drawing.Point(12, 59);
+            this.btnEvaluate.Name = "btnEvaluate";
+            this.btnEvaluate.Size = new System.Drawing.Size(75, 23);
+            this.btnEvaluate.TabIndex = 2;
+            this.btnEvaluate.Text = "Evaluate";
+            this.btnEvaluate.UseVisualStyleBackColor = true;
+            this.btnEvaluate.Click += new System.EventHandler(this.btnEvaluate_Click);
             // 
             // lbResult
             // 
             this.lbResult.AutoSize = true;
-            this.lbResult.Location = new System.Drawing.Point(7, 253);
+            this.lbResult.Location = new System.Drawing.Point(12, 86);
             this.lbResult.Name = "lbResult";
-            this.lbResult.Size = new System.Drawing.Size(220, 13);
-            this.lbResult.TabIndex = 5;
-            this.lbResult.Text = "Results in System.Diagnostics.Watch ticks";
+            this.lbResult.Size = new System.Drawing.Size(39, 13);
+            this.lbResult.TabIndex = 1;
+            this.lbResult.Text = "Result";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 399);
-            this.Controls.Add(this.gbFindList);
+            this.ClientSize = new System.Drawing.Size(697, 141);
+            this.Controls.Add(this.btnEvaluate);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.lbExpression);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbExpression);
             this.Name = "MainForm";
-            this.Text = "Примеры";
-            this.gbFindList.ResumeLayout(false);
-            this.gbFindList.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numFLCount)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numFLMaxLen)).EndInit();
+            this.Text = "Test Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox gbFindList;
-        private System.Windows.Forms.Button btnFLTest;
-        private System.Windows.Forms.ListBox lxFLSource;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.NumericUpDown numFLMaxLen;
-        private System.Windows.Forms.NumericUpDown numFLCount;
+        private System.Windows.Forms.TextBox tbExpression;
+        private System.Windows.Forms.Label lbExpression;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnEvaluate;
         private System.Windows.Forms.Label lbResult;
-        private System.Windows.Forms.TextBox tbFLResult;
+
     }
 }
 
--- Samples/MainForm.cs
+++ Samples/MainForm.cs
@@ -20,87 +20,35 @@ namespace Samples
         }
         #endregion
 
-        #region Find List test
-        private void btnFLTest_Click(object sender, EventArgs e)
+        private void btnEvaluate_Click(object sender, EventArgs e)
         {
-            this.lxFLSource.Items.Clear();
-            this.lxFLSource.BeginUpdate();
-            int maxLen = Convert.ToInt32(this.numFLMaxLen.Value);
-            for (int i = 0; i < this.numFLCount.Value; i++)
-                this.lxFLSource.Items.Add(getRandomString(maxLen));
-            this.lxFLSource.EndUpdate();
-            Application.DoEvents();
-            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
-            StringBuilder sb = new StringBuilder();
-           
-            sb.AppendLine("Addding [ticks/bytes].");
-            long tm = GC.GetTotalMemory(true);
-            List<string> list = new List<string>(this.lxFLSource.Items.Count);
-            watch.Reset();
-            watch.Start();
-            for (int i = 0; i < this.lxFLSource.Items.Count; i++)
+            object result;
+            try
             {
-                list.Add((string)this.lxFLSource.Items[i]);
-            }
-            watch.Stop();
-            sb.Append("List: ");
-            sb.Append(watch.ElapsedTicks.ToString());
-            tm = GC.GetTotalMemory(true) - tm;
-            sb.Append('/');
-            sb.Append(tm.ToString());
-
-            tm = GC.GetTotalMemory(true);
-            watch.Reset();
-            watch.Start();
-            HashSet<string> hset = new HashSet<string>(list);
-            watch.Stop();
-            sb.Append("   HashSet: ");
-            sb.Append(watch.ElapsedTicks.ToString());
-            tm = GC.GetTotalMemory(true) - tm;
-            sb.Append('/');
-            sb.Append(tm.ToString());
-
-            sb.AppendLine();
-            sb.AppendLine("Finding [ticks].");
-            watch.Reset();
-            watch.Start();
-            for (int i = 0; i < this.lxFLSource.Items.Count; i++)
-            {
-                list.IndexOf((string)this.lxFLSource.Items[i]);
-            }
-            watch.Stop();
-            sb.Append("List: ");
-            sb.Append(watch.ElapsedTicks.ToString());
-
-            watch.Reset();
-            watch.Start();
-            for (int i = 0; i < this.lxFLSource.Items.Count; i++)
-            {
-                hset.Contains((string)this.lxFLSource.Items[i]);
-            }
-            watch.Stop();
-            sb.Append("   HashSet: ");
-            sb.Append(watch.ElapsedTicks.ToString());
-
-
-
-            this.tbFLResult.Text = sb.ToString();
-        }
-        #endregion
-
-        #region Random string generator
-        private readonly Random rnd = new Random();
-        
-        private const string rndChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+                result = SnowLib.Scripting.SimpleExpressionParser.GetValue(this.tbExpression.Text, null);
                 
-        private string getRandomString(int maxLength)
-        {
-            int size = Math.Max(this.rnd.Next(maxLength), 5);
-            char[] buffer = new char[size];
-            for (int i = 0; i < size; i++)
-                buffer[i] = rndChars[this.rnd.Next(rndChars.Length)];
-            return new string(buffer);
+            }
+            catch(SnowLib.Scripting.SimpleExpressionException see)
+            {
+                if (see.Position >= 0)
+                {
+                    this.tbExpression.Select(see.Position, see.Length);
+                    this.tbExpression.Focus();
+                }
+                this.tbResult.ForeColor = Color.Red;
+                this.tbResult.Text = see.Message;
+                return;
+            }
+            catch(Exception ex)
+            {
+                this.tbResult.ForeColor = Color.Red;
+                this.tbResult.Text = ex.Message;
+                return;
+
+            }
+            this.tbResult.ForeColor = SystemColors.WindowText;
+            this.tbResult.Text = Convert.ToString(result);
+
         }
-        #endregion
     }
 }
